4c7eb21ab23831de7f7645a39bbc8c9f4dfc634b
naohisa0616 <naohisa0616@icloud.com>
2019-12-08 17:27:00 +0900

    ComputedプロパティareaをもったCircleクラス →式を介して読み書きします。

--- Circle/ViewController.swift
+++ Circle/ViewController.swift
@@ -8,6 +8,25 @@
 
 import UIKit
 
+class Circle {
+//    半径
+    var radius:Double = 1.0
+//    面積
+    var area:Double {
+//        areaの値を計算して、面積を返す
+        get{
+            return radius * radius * Double.pi
+        }
+//        面積を設定する（半径を設定する）
+        set (menseki){
+//            areaの値を設定すると見せて、実際はradiusの値を設定します
+            radius = sqrt(menseki / Double.pi)
+        }
+    }
+    
+    
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
