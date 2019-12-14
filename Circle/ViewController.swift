54ed682820ac701228993e9acca2a43eb3d4c958
naohisa0616 <naohisa0616@icloud.com>
2019-12-14 13:59:24 +0900

    Circleクラスで円を作って面積を2倍に設定する。 ViewControollerの中に処理を書く

--- Circle/ViewController.swift
+++ Circle/ViewController.swift
@@ -9,11 +9,11 @@
 import UIKit
 
 class Circle {
-//    半径
+//    半径(プロパティ)
     var radius:Double = 1.0
-//    面積
+//    面積（メソッド）
     var area:Double {
-//        areaの値を計算して、面積を返す
+//        面積を返す
         get{
             return radius * radius * Double.pi
         }
@@ -23,15 +23,29 @@ class Circle {
             radius = sqrt(menseki / Double.pi)
         }
     }
-    
-    
 }
 
+//Viewを管理・操作（表示・非表示・配置・アニメショーンなど）をする役割を持つクラス
+//受け取ったTextやViewを表示・管理するクラス
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //  円を作る
+            let myCircle = Circle()
+        //  初期値の半径と面積
+            print("半径\(myCircle.radius)")
+            print("半径\(myCircle.area)")
+        
+        //  面積を2倍にする
+            myCircle.area *= 2
+            print("半径\(myCircle.radius)")
+            print("半径\(myCircle.area)")
+        
+        //  半径を3.0にする
+        myCircle.radius = 3.0
+            print("半径\(myCircle.radius)")
+            print("半径\(myCircle.area)")
     }
 
 
